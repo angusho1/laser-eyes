61722d877148df70f500ac43d5538bad2082dce8
angusho1 <57027339+angusho1@users.noreply.github.com>
2022-07-31 23:05:09 -0700

    Get image data upload working

--- laser_eyes/urls.py
+++ laser_eyes/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('process', views.process, name='process'),
     path('', views.index, name='index')
 ]
--- laser_eyes/views.py
+++ laser_eyes/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.decorators.csrf import csrf_exempt
 
 from django.http import Http404, HttpResponse, HttpRequest
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, 'index.html')
+
+@csrf_exempt
+def process(request: HttpRequest) -> HttpResponse:
+    print(request.body)
+    return HttpResponse('success')
